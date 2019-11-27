80a6ae0a9f00efca7725e71fe62a8ab403686d0a
Roberto <roberto.amato.11.93@gmail.com>
2019-11-27 20:10:16 +0100

    script creazione utente mysql, creazione database e assegnazione permessi sul database all'utente

---
+++ create_user_database.sql
@@ -0,0 +1,12 @@
+# 1) CREAZIONE DATABASE 
+# 2) CREAZIONE DI UNA NUOVA UTENZA MYSQL
+# 3) ASSEGNAZIONE PROVILEGI DATABASE CREATO NEL PUNTO 1 PER L UTENZA CREATA AL PUNTO 2
+# https://www.tosolini.info/2015/10/mysql-creare-utentedb-e-assegnare-permessi-da-command-line/
+# https://www.mrwebmaster.it/mysql/gestire-utenti-permessi-grant-revoke_7041.html 
+
+CREATE DATABASE centauri_db;
+SHOW DATABASES;
+CREATE USER 'centauri'@'localhost' IDENTIFIED BY 'centauri';
+GRANT ALL ON centauri_db.* TO 'centauri'@'localhost';
+SHOW GRANTS FOR 'centauri'@'localhost';
+FLUSH PRIVILEGES;
