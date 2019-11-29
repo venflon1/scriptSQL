27441f91275a36d8e369df4291415b493f886f6d
venflon1 <roberto.amato.11.93@gmail.com>
2019-11-29 12:34:44 +0000

    FYI

--- create_user_database.sql
+++ create_user_database.sql
@@ -2,7 +2,7 @@
 # 2) CREAZIONE DI UNA NUOVA UTENZA MYSQL
 # 3) ASSEGNAZIONE PROVILEGI DATABASE CREATO NEL PUNTO 1 PER L UTENZA CREATA AL PUNTO 2
 # https://www.tosolini.info/2015/10/mysql-creare-utentedb-e-assegnare-permessi-da-command-line/
-# https://www.mrwebmaster.it/mysql/gestire-utenti-permessi-grant-revoke_7041.html 
+# https://www.mrwebmaster.it/mysql/gestire-utenti-permessi-grant-revoke_7041.html
 
 CREATE DATABASE centauri_db;
 SHOW DATABASES;
